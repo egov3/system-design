29996e33eb0359fa95dc955d7f886d1ad18e6903
Zhassulan Baigozha <zhassulan.baigozha@nitec.kz>
2024-11-06 20:00:12 +0500

    fix: [egov-0806] egov3 system-design

--- dist/jest.config.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Config } from '@jest/types';
-declare const _default: () => Promise<Config.InitialProjectOptions>;
-export default _default;
-//# sourceMappingURL=jest.config.d.ts.map
--- src/svg/ClearIcon.d.ts
+++ src/svg/ClearIcon.d.ts
@@ -1,5 +1,5 @@
 import React from "react";
 export declare const ClearIcon: ({ width, height, fill, pathFill, }: React.SVGProps<SVGSVGElement> & {
     pathFill?: string;
-}) => import("react/jsx-runtime").JSX.Element;
+}) => any;
 //# sourceMappingURL=ClearIcon.d.ts.map
