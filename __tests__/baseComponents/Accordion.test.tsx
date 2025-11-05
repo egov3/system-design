603068f1d0c292409fbd17cd153e2f90297fde44
Grigoriy Ponomarev <grigoriy.ponomarev@nitec.kz>
2025-11-05 21:43:47 +0500

    test: [10000000] RadioGroup test

--- __tests__/baseComponents/Accordion.test.tsx
+++ __tests__/baseComponents/Accordion.test.tsx
@@ -1,20 +1,23 @@
-import { fireEvent, render, renderHook, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { useState } from "react";
 import { BaseComponents } from "~baseComponents";
 
+const TestWrapper = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <BaseComponents.Accordion
+      open={open}
+      setOpen={setOpen}
+      title={<>Accordion title</>}
+    >
+      Accordion Content
+    </BaseComponents.Accordion>
+  );
+};
+
 describe("Accordion", () => {
   it("(1) Should toggle accordion when clicking on the header", () => {
-    const { result } = renderHook(() => useState(false)); // NOSONAR
-
-    const { rerender } = render(
-      <BaseComponents.Accordion
-        open={result.current[0]}
-        setOpen={result.current[1]}
-        title={<>Accordion title</>}
-      >
-        Accordion Content
-      </BaseComponents.Accordion>,
-    );
+    render(<TestWrapper />);
 
     const accordionHeader = screen.getByTestId("Accordion_BUTTON");
     const accordionContent = screen.getByTestId("Accordion_CONTENT");
@@ -22,16 +25,6 @@ describe("Accordion", () => {
     expect(accordionContent).toHaveClass("accordionContent--hidden");
 
     fireEvent.click(accordionHeader);
-
-    rerender(
-      <BaseComponents.Accordion
-        open={result.current[0]}
-        setOpen={result.current[1]}
-        title={<>Accordion title</>}
-      >
-        Accordion Content
-      </BaseComponents.Accordion>,
-    );
 
     expect(accordionContent).not.toHaveClass("accordionContent--hidden");
   });
