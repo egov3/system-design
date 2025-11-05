d7e6fe3237750742b364f86eaf5b0c0668b35c73
Grigoriy Ponomarev <grigoriy.ponomarev@nitec.kz>
2025-11-05 22:01:03 +0500

    test: [10000000] RadioToggle test

--- __tests__/baseComponents/RadioGroup.test.tsx
+++ __tests__/baseComponents/RadioGroup.test.tsx
@@ -6,17 +6,19 @@ const radioGroupItems = [
   { label: "Option 2", value: "option2" },
 ];
 
-const setSelectedOption=jest.fn();
+const setSelectedOption = jest.fn();
 
 describe("RadioGroup", () => {
   it("(1) Should check option if clicked", () => {
-    render(<BaseComponents.RadioGroup
-      radioGroupItems={radioGroupItems}
-      setSelectedOption={setSelectedOption}
-      selectedOption={radioGroupItems[0].value}
-    />);
-    
-    const options =screen.getAllByTestId("RadioGroupItem_LABEL");
+    render(
+      <BaseComponents.RadioGroup
+        radioGroupItems={radioGroupItems}
+        setSelectedOption={setSelectedOption}
+        selectedOption={radioGroupItems[0].value}
+      />,
+    );
+
+    const options = screen.getAllByTestId("RadioGroupItem_LABEL");
 
     fireEvent.click(options[1]);
 
