c3239941c183aa680268e34a74737e2dc5742da4
Grigoriy Ponomarev <grigoriy.ponomarev@nitec.kz>
2025-11-06 09:14:16 +0500

    test: [100001683] RadioGroup tests

---
+++ __tests__/baseComponents/RadioGroup.test.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { BaseComponents } from "~baseComponents";
+
+const radioGroupItems = [
+  { label: "Option 1", value: "option1" },
+  { label: "Option 2", value: "option2" },
+];
+
+const setSelectedOption = jest.fn();
+
+describe("RadioGroup", () => {
+  it("(1) Should check option if clicked", () => {
+    render(
+      <BaseComponents.RadioGroup
+        radioGroupItems={radioGroupItems}
+        setSelectedOption={setSelectedOption}
+        selectedOption={radioGroupItems[0].value}
+      />,
+    );
+
+    const options = screen.getAllByTestId("RadioGroupItem_LABEL");
+
+    fireEvent.click(options[1]);
+
+    expect(setSelectedOption).toHaveBeenCalledWith("option2");
+  });
+});
