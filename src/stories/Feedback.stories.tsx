ea687f97df8a79365ce7412f8f87e743a6e03155
balkiyaeva <amil.balkiyaeva@gmail.com>
2025-11-14 10:03:56 +0500

    feat: [10001443] Feedback handle rating, storybook

--- src/stories/Feedback.stories.tsx
+++ src/stories/Feedback.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-webpack5";
+import { useState } from "react";
 import { Components } from "~components";
 
 const meta = {
@@ -9,9 +10,36 @@ const meta = {
   },
   tags: ["autodocs"],
   argTypes: {},
+  args: {
+    lang: "ru",
+    onAction: () => {},
+    rating: 5,
+    value: "",
+    onChange: () => {},
+    setRating: () => {},
+  },
 } satisfies Meta<typeof Components.Feedback>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args) => {
+    const InteractiveComponent = () => {
+      const [rating, setRating] = useState(5);
+      const [feedbackText, setFeedbackText] = useState("");
+
+      return (
+        <Components.Feedback
+          {...args}
+          rating={rating}
+          setRating={setRating}
+          value={feedbackText}
+          onChange={(e) => setFeedbackText(e.target.value)}
+        />
+      );
+    };
+
+    return <InteractiveComponent />;
+  },
+};
