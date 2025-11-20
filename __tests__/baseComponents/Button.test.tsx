d841b90e307f94c34307f76abb5a4fc6fdfc3bf0
Ablay.Yelshibayev <Ablay.Yelshibayev@nitec.kz>
2025-11-20 11:12:28 +0500

    feat: [10001444] test Button

--- __tests__/baseComponents/Button.test.tsx
+++ __tests__/baseComponents/Button.test.tsx
@@ -1,23 +1,30 @@
+import { Icons } from "@egov3/graphics";
 import { render, screen } from "@testing-library/react";
 import { BaseComponents } from "~baseComponents";
 
 describe("Button", () => {
+  const getButton = () => screen.getByTestId("Button_MAIN");
   it("(1) Should render rounded button", () => {
-    render(
-      <BaseComponents.Button isRounded={true}>Click me</BaseComponents.Button>,
-    );
-    expect(screen.getByText("Click me")).toHaveClass("btn-rounded--medium");
+    render(<BaseComponents.Button isRounded>Click me</BaseComponents.Button>);
+    expect(getButton()).toHaveClass("btn-rounded--medium");
   });
 
   it("(2) Should render square button", () => {
     render(<BaseComponents.Button>Click me</BaseComponents.Button>);
-    expect(screen.getByText("Click me")).toHaveClass("btn-square--medium");
+    expect(getButton()).toHaveClass("btn-square--medium");
   });
 
   it("(3) Should render disabled button", () => {
+    render(<BaseComponents.Button disabled>Click me</BaseComponents.Button>);
+    expect(getButton()).toHaveClass("btn-default--disabled");
+  });
+
+  it("(4) Should apply icon styles when isIcon is true", () => {
     render(
-      <BaseComponents.Button disabled={true}>Click me</BaseComponents.Button>,
+      <BaseComponents.Button isIcon>
+        <Icons.Additional.SettingsOutline width={24} height={24} fill="#fff" />
+      </BaseComponents.Button>,
     );
-    expect(screen.getByText("Click me")).toHaveClass("btn-default--disabled");
+    expect(getButton()).toHaveClass("btn--medium--icon");
   });
 });
