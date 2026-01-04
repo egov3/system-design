1de35f89f43115022bf6648e1087fb6b904f6032
Ablay.Yelshibayev <Ablay.Yelshibayev@nitec.kz>
2026-01-04 14:38:30 +0500

    feat: [10001973] upd ModalFooterButton

--- src/baseComponents/ModalFooterButton/index.tsx
+++ src/baseComponents/ModalFooterButton/index.tsx
@@ -13,20 +13,18 @@ export interface IModalFooterButtonProps {
   buttonList: ModalFooterButtonItem[];
 }
 
-export const ModalFooterButton = ({ buttonList }: IModalFooterButtonProps) => {
-  return (
-    <div data-testid="ModalFooterButton_WRAP" className={styles.wrapper}>
-      {buttonList.map(({ text, ...buttonProps }) => (
-        <BaseComponents.Button
-          key={text}
-          aria-label={text}
-          data-testid="ModalFooter_BUTTON"
-          size="large"
-          {...buttonProps}
-        >
-          {text}
-        </BaseComponents.Button>
-      ))}
-    </div>
-  );
-};
+export const ModalFooterButton = ({ buttonList }: IModalFooterButtonProps) => (
+  <div data-testid="ModalFooterButton_WRAP" className={styles.wrapper}>
+    {buttonList.map(({ text, ...buttonProps }) => (
+      <BaseComponents.Button
+        key={text}
+        aria-label={text}
+        data-testid="ModalFooter_BUTTON"
+        size="large"
+        {...buttonProps}
+      >
+        {text}
+      </BaseComponents.Button>
+    ))}
+  </div>
+);
