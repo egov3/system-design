536421b0e27ebef5bf910d856e93391f58bd9d43
Ablay.Yelshibayev <Ablay.Yelshibayev@nitec.kz>
2026-01-26 23:12:49 +0500

    feat: [10002203] unit test

---
+++ __tests__/components/PresaleComponent/ServiceDetails.test.tsx
@@ -0,0 +1,47 @@
+import "@testing-library/jest-dom";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Components } from "~components";
+
+describe("ServiceDetails.ShowPassportBtn", () => {
+  it("(1) Should render toggles showPassport state when clicked", () => {
+    const setShowPassport = jest.fn();
+    const showPassport = false;
+
+    render(
+      <Components.PresaleComponent.ShowPassportBtn
+        setShowPassport={setShowPassport}
+        showPassport={showPassport}
+        lang="ru"
+      />,
+    );
+
+    const button = screen.getByText("Подробный паспорт услуги");
+    fireEvent.click(button);
+
+    expect(setShowPassport).toHaveBeenCalledWith(true);
+  });
+
+  it('(2) Should render when serviceId="P305"', () => {
+    const { getByText } = render(
+      <Components.PresaleComponent.ServiceDetails serviceId="P305" lang="ru" />,
+    );
+    expect(
+      getByText(
+        "Предоставление сведений о зарегистрированных правах (обременениях) на недвижимое имущество и его технических характеристиках",
+      ),
+    ).toHaveTextContent(
+      "Предоставление сведений о зарегистрированных правах (обременениях) на недвижимое имущество и его технических характеристиках",
+    );
+  });
+
+  it('(3) Should show passport details when serviceId="P305"', () => {
+    render(
+      <Components.PresaleComponent.ServiceDetails serviceId="P305" lang="ru" />,
+    );
+
+    fireEvent.click(screen.getByTestId("ShowPassport_BTN"));
+
+    expect(screen.getByTestId("PassportDetails_WRAPPER")).toBeInTheDocument();
+  });
+});
--- src/components/Presale/AccordionTitle/index.tsx
+++ src/components/Presale/AccordionTitle/index.tsx
@@ -1,7 +1,7 @@
 import { BaseComponents } from "~baseComponents";
 import styles from "./AccordionTitle.module.css";
 
-export const AccordionTitle = (title: string) => (
+export const AccordionTitle = ({ title }: { title: string }) => (
   <BaseComponents.Typography
     aria-label={title}
     className={styles.title}
