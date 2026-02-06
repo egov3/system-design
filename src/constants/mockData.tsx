26816b924f29308b28b91b765933ab3fb1ebe57a
Ablay.Yelshibayev <Ablay.Yelshibayev@nitec.kz>
2026-02-06 07:16:39 +0500

    chore: [10002203] added up to 10 steps

--- src/constants/mockData.tsx
+++ src/constants/mockData.tsx
@@ -1,7 +1,5 @@
-import type {
-  IPassportDetailsItem,
-  IServiceDetailsItem,
-} from "~interfaces/PresaleTemplate";
+import type { IPassportDetailsItem } from "../components/Presale/PassportDetails";
+import type { IServiceDetailsItem } from "../components/Presale/ServiceDetails";
 
 export const accordion = {
   ru: "Акордеон",
@@ -13,6 +11,13 @@ export const steps = [
   { ru: "Шаг 1", kk: "Қадам 1", en: "Step 1" },
   { ru: "Шаг 2", kk: "Қадам 2", en: "Step 2" },
   { ru: "Шаг 3", kk: "Қадам 3", en: "Step 3" },
+  { ru: "Шаг 4", kk: "Қадам 4", en: "Step 4" },
+  { ru: "Шаг 5", kk: "Қадам 5", en: "Step 5" },
+  { ru: "Шаг 6", kk: "Қадам 6", en: "Step 6" },
+  { ru: "Шаг 7", kk: "Қадам 7", en: "Step 7" },
+  { ru: "Шаг 8", kk: "Қадам 8", en: "Step 8" },
+  { ru: "Шаг 9", kk: "Қадам 9", en: "Step 9" },
+  { ru: "Шаг 10", kk: "Қадам 10", en: "Step 10" },
 ];
 
 export const passportDetails: IPassportDetailsItem[] = [
