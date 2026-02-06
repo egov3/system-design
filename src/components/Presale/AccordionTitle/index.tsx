e0ef49e9513e75e89833633002c5e8f8291aad95
Ablay.Yelshibayev <Ablay.Yelshibayev@nitec.kz>
2026-02-06 07:19:13 +0500

    chore: [10002203] add interface AccordionTitle

--- src/components/Presale/AccordionTitle/index.tsx
+++ src/components/Presale/AccordionTitle/index.tsx
@@ -1,7 +1,11 @@
 import { BaseComponents } from "~baseComponents";
 import styles from "./AccordionTitle.module.css";
 
-export const AccordionTitle = ({ title }: { title: string }) => (
+interface IAccordionTitleProps {
+  title: string;
+}
+
+export const AccordionTitle = ({ title }: IAccordionTitleProps) => (
   <BaseComponents.Typography
     aria-label={title}
     className={styles.title}
