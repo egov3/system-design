3d07a7db7233233e1a8f9a5fc77d37243160c176
Amira.Balkiyayeva <amil.balkiyaeva@gmail.com>
2026-05-26 08:34:47 +0500

    feat: [10002449] add storybook

---
+++ src/stories/Components/Notification/NotificationComponent.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from "@storybook/react-webpack5";
+import { Components } from "~components";
+
+const meta: Meta<typeof Components.NotificationItem> = {
+  title: "Components/Notification/NotificationItem",
+  component: Components.NotificationItem,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          height: "80px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    type: {
+      control: "radio",
+      options: ["success", "error", "warning", "info"],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Components.NotificationItem>;
+
+export const Success: Story = {
+  args: {
+    text: "Success notification",
+    type: "success",
+  },
+};
+
+export const ErrorState: Story = {
+  args: {
+    text: "Error notification",
+    type: "error",
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    text: "Warning notification",
+    type: "warning",
+  },
+};
+
+export const Info: Story = {
+  args: {
+    text: "Info notification",
+    type: "info",
+  },
+};
