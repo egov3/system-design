851ec677305a604ffceadf290ed4a2819fd2fe71
Maxim Yenbakhtov <enbachtov@gmail.com>
2026-06-21 13:42:49 +0500

    feat: [10003051] add search feedback reason controls

--- src/constants/i18n/SearchQualityFeedback.tsx
+++ src/constants/i18n/SearchQualityFeedback.tsx
@@ -9,4 +9,19 @@ export const SearchQualityFeedback = {
     kk: "Сіздің пікіріңіз біз үшін өте маңызды.\nМәселені мүмкіндігінше толық сипаттаңыз.",
     en: "Your feedback is very important to us.\nDescribe the issue in as much detail as possible.",
   },
+  reasonPlaceholder: {
+    ru: "Напишите отзыв",
+    kk: "Пікір жазыңыз",
+    en: "Write feedback",
+  },
+  submit: {
+    ru: "Отправить",
+    kk: "Жіберу",
+    en: "Send",
+  },
+  cancel: {
+    ru: "Отменить",
+    kk: "Болдырмау",
+    en: "Cancel",
+  },
 };
