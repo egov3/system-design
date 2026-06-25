0eb2b872b5b61233d05867069a5021deaa224f02
Maxim Yenbakhtov <enbachtov@gmail.com>
2026-06-25 15:59:56 +0500

    feat: [10003051] add quality feedback service tests

--- __tests__/components/Services/QualityFeedback.test.tsx
+++ __tests__/components/Services/QualityFeedback.test.tsx
@@ -51,4 +51,52 @@ describe("QualityFeedback", () => {
 
     expect(onLowRating).toHaveBeenCalledWith(1);
   });
+
+  it("(3) Should render custom texts", () => {
+    const texts = {
+      submitButtonText: "Оценить",
+      subtitle: "Кастомное описание",
+      successButtonText: "Повторить",
+      successSubtitle: "Кастомное описание успеха",
+      successTitle: "Спасибо",
+      title: "Кастомный заголовок",
+    };
+
+    render(
+      <Components.QualityFeedback
+        lang={lang}
+        onLowRating={jest.fn()}
+        {...texts}
+      />,
+    );
+
+    expect(screen.getByText(texts.title)).toBeInTheDocument();
+    expect(screen.getByText(texts.subtitle)).toBeInTheDocument();
+    expect(screen.getByText(texts.submitButtonText)).toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId("QualityFeedback_SUBMIT_BUTTON"));
+
+    expect(screen.getByText(texts.successTitle)).toBeInTheDocument();
+    expect(screen.getByText(texts.successSubtitle)).toBeInTheDocument();
+    expect(screen.getByText(texts.successButtonText)).toBeInTheDocument();
+  });
+
+  it("(4) Should render controlled success mode", () => {
+    const onReset = jest.fn();
+
+    render(
+      <Components.QualityFeedback
+        isSuccessMode
+        lang={lang}
+        onLowRating={jest.fn()}
+        onReset={onReset}
+      />,
+    );
+
+    expect(screen.getByTestId("QualityFeedback_THANKS")).toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId("QualityFeedback_RESET_BUTTON"));
+
+    expect(onReset).toHaveBeenCalledTimes(1);
+  });
 });
