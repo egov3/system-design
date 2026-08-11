7fc91bc7cd0385f6a97cb96306b3858ae1a01b15
Gregory Ponomaryov <pvg9kz@gmail.com>
2026-08-11 09:19:21 +0500

    feat: [10005493] presale components fix (#226)
    * feat: [10005493] instruction component fix
    
    * fix: [10005493] service details update
    
    ---------
    
    Co-authored-by: Grigoriy Ponomarev <grigoriy.ponomarev@nitec.kz>

--- __tests__/components/PresaleComponent/ServiceDetails.test.tsx
+++ __tests__/components/PresaleComponent/ServiceDetails.test.tsx
@@ -4,35 +4,33 @@ import { htmlText } from "~constants/mock";
 import { passportDetails, serviceDetails } from "~constants/mockData";
 
 describe("PresaleComponent.ServiceDetails", () => {
-  it("(1) Should render service label", () => {
+  it("(1) Should show passport details after button click", () => {
     render(
       <PresaleComponent.ServiceDetails
         passportDetails={passportDetails}
         servicesDetails={serviceDetails}
         lang="ru"
-        howItWorksText={htmlText}
-      />,
-    );
-    expect(screen.getAllByTestId("ServiceDetails_TITLE")[0]).toHaveTextContent(
-      "Заголовок 1",
-    );
-    expect(
-      screen.getAllByTestId("ServiceDetails_DESCRIPTION")[0],
-    ).toHaveTextContent("Подзаголовок 1");
-  });
-
-  it("(2) Should show passport details after button click", () => {
-    render(
-      <PresaleComponent.ServiceDetails
-        passportDetails={passportDetails}
-        servicesDetails={serviceDetails}
-        lang="ru"
-        howItWorksText={htmlText}
+        howItWorksText={""}
       />,
     );
 
     fireEvent.click(screen.getByTestId("ShowPassport_BTN"));
 
     expect(screen.getByTestId("PassportDetails_WRAPPER")).toBeInTheDocument();
+  });
+
+  it("(2) Should show how it works modal after button click", () => {
+    render(
+      <PresaleComponent.ServiceDetails
+        passportDetails={passportDetails}
+        servicesDetails={serviceDetails}
+        lang="ru"
+        howItWorksText={htmlText}
+      />,
+    );
+
+    fireEvent.click(screen.getByTestId("HowItWorks_BTN"));
+
+    expect(screen.getByTestId("HowItWorks_WRAPPER")).toBeInTheDocument();
   });
 });
